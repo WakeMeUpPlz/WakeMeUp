4d785546856216faa787c1faba0ecb916898fac2
arimkim <kal72222222@gmail.com>
2022-12-28 00:34:58 +0900

    test : add test button add temporary button

--- app/src/main/java/com/example/wakemeup/auth/IntroActivity.kt
+++ app/src/main/java/com/example/wakemeup/auth/IntroActivity.kt
@@ -25,5 +25,10 @@ class IntroActivity : AppCompatActivity() {
             val intent = Intent(this, JoinActivity::class.java)
             startActivity(intent)
         }
+
+        binding.btn.setOnClickListener {
+            val intent = Intent(this, AlarmListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
