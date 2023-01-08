f57f73a45d99f2e5f980f56ce225017e817909b8
arimkim <kal72222222@gmail.com>
2023-01-08 17:15:36 +0900

    Edit : edit server connection interface

--- app/src/main/java/com/example/wakemeup/retrofit/Retrofit_interface.kt
+++ app/src/main/java/com/example/wakemeup/retrofit/Retrofit_interface.kt
@@ -21,20 +21,21 @@ interface Retrofit_interface {
 
     @FormUrlEncoded
     @POST("/alarmRegister")
-    fun alarmRegsiter(@Field("alarmNum") alarmNum : Int,@Field("title") title : String,
+    fun alarmRegsiter(@Field("alarmNum") alarmNum : Int, @Field("title") title : String,
                       @Field("helper") helper : String,@Field("dorN") dorN : String,
                       @Field("isActivated") isActivated : Int,@Field("isHelperActivated") isHelperActivated : Int,
                       @Field("ringTone") ringTone : String, @Field("id") id : String,
                       @Field("alarmTime") alarmTime : String, @Field("dates") dates : String): Call<ResponseData>
 
+    @FormUrlEncoded
+    @POST("/alarmDelete")
+    fun alarmDelete(@Field("alarmNum") alarmNum : Int, @Field("id") id : String): Call<ResponseData>
 
-    @DELETE("/alarmDelete")
-    fun alarmDelete(@Query("alarmNum") alarmNum : Int, @Query("id") id : String): Call<ResponseData>
-
-    @PUT("/alarmUpdate")
-    fun alarmUpdate(@Query("alarmNum") alarmNum : Int,@Query("title") title : String,
-                      @Query("helper") helper : String,@Query("dorN") dorN : String,
-                      @Query("isActivated") isActivated : Int,@Query("isHelperActivated") isHelperActivated : Int,
-                      @Query("ringTone") ringTone : String, @Query("id") id : String,
-                      @Query("alarmTime") alarmTime : String, @Query("dates") dates : String): Call<ResponseData>
+    @FormUrlEncoded
+    @POST("/alarmUpdate")
+    fun alarmUpdate(@Field("alarmNum") alarmNum : Int,@Field("title") title : String,
+                      @Field("helper") helper : String,@Field("dorN") dorN : String,
+                      @Field("isActivated") isActivated : Int,@Field("isHelperActivated") isHelperActivated : Int,
+                      @Field("ringTone") ringTone : String, @Field("id") id : String,
+                      @Field("alarmTime") alarmTime : String, @Field("dates") dates : String): Call<ResponseData>
 }
